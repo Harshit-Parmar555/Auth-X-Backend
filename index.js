c546ade86f994ebf679ff3b31a2158be40a2c270
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-13 22:55:04 +0530

    chore: setup server entry point with index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+// Database connection function
+import { connectDb } from "./src/db/connect.js";
+
+// Express App
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Logger config
+import morgan from "morgan";
+import logger from "./src/utils/logger.js";
+
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// App listening
+connectDb()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
