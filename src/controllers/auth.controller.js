046f3012d81e92ac4cb6651aec2941b81a851b99
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-14 15:03:50 +0530

    feat: add logout controller and setup logout route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -108,3 +108,16 @@ export const login = async (req, res) => {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
+
+// Logout controller
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
