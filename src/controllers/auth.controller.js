3adc8e552f68af604794c3e2bbb2f2a0495e9498
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-14 14:56:35 +0530

    feat: add signup controller and setup signup route

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,62 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+// Signup controller
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please provide all fields" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await newUser.save();
+
+    const token = await generateJwt(newUser._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    await sendVerificationEmail(newUser.email, verificationToken);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "User Created Successfully" });
+  } catch (error) {
+    if (error.code === 11000) {
+      // Handling MongoDB duplicate key error
+      const field = Object.keys(error.keyPattern)[0];
+      return res.status(400).json({
+        success: false,
+        message: `${
+          field === "username" ? "Username" : "Email"
+        } is already taken. Try a different one.`,
+      });
+    }
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
